938126212695bd10f2fa137b1950ceb869024512
david.silva <David.Silva@meraki.net>
2022-02-16 15:02:08 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'cde35da15ebaddc85c6422fe6d3048c351d268c3664e620d1d0fdbd3a95afb1079ad79d7ca3540ab6f512eaccd39fb0626a9d5b1a4a64390eb41be724471f4f0'
